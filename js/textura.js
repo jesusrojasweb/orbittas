9ec44e0439c1e9deabdee9e0d103d872a5075684
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-07-15 18:32:29 -0400

    Se edito la textura

--- js/textura.js
+++ js/textura.js
@@ -1,18 +1,17 @@
 let tamano = 10
+let letras = ['a','b', 'c', 'd']
 let logo = []
 
-for(let i = 0; i < 10; i++){
-	logo[i] = document.getElementById(i+1)
+for(let i = 0; i <= 4; i++){
+	logo[i] = document.querySelector("." + letras[i])
 }
-console.log(logo)
 function aleatorio(min, max) {
 	return Math.round(Math.random() * (max - min)) + min
 }
 setInterval(relleno, 2000)
 
 function relleno() {
-	let numero = aleatorio(0,10)
-	console.log(numero)
+	let numero = aleatorio(0,4)
 	setTimeout(()=>toggleClass(logo[numero]), 1000)
 	toggleClass(logo[numero])
 }
