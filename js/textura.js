227009dc576ddd402834b515296a14f6baf2fccb
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-07-18 10:44:07 -0400

    Se hizo responsive el contacto nuevo

--- js/textura.js
+++ js/textura.js
@@ -3,7 +3,7 @@ let letras = ['a','b', 'c', 'd']
 let logo = []
 
 for(let i = 0; i <= 4; i++){
-	logo[i] = document.querySelectorAll("." + letras[i])
+	logo[i] = document.querySelector("." + letras[i])
 }
 function aleatorio(min, max) {
 	return Math.round(Math.random() * (max - min)) + min
@@ -13,12 +13,10 @@ setInterval(relleno, 2000)
 function relleno() {
 	let numero = aleatorio(0,4)
 	setTimeout(()=>{
-		toggleClass(logo[numero][0])
-		toggleClass(logo[numero][1])
+		toggleClass(logo[numero])
 
 	}, 1000)
-	toggleClass(logo[numero][0])
-	toggleClass(logo[numero][1])
+	toggleClass(logo[numero])
 }
 
 function toggleClass(item) {
