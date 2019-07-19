12a1326013346a42acfadd98af8d9d4ba1a5a3ee
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-07-19 09:26:48 -0400

    Se centraron los iconos de valores y tambien el menu del portafolio y contacto se cambian cuando se hace scroll

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,22 @@
 const $header = document.getElementById('header')
 const $burger = document.getElementById('burger')
 const $close = document.getElementById('close')
+const $hero = document.querySelector('.contacto') || document.querySelector('.portafolio')
 $burger.addEventListener('click', function (e) {
   $header.classList.add('activo')
 })
 $close.addEventListener('click', function (e) {
   $header.classList.remove('activo')
+})
+
+window.addEventListener('scroll', function (e) {
+  let coordsHero = $hero.getBoundingClientRect()
+  
+  if(coordsHero.top > -100){
+    $header.classList.remove('scroll')
+  }
+  if(coordsHero.top <= -100){
+    $header.classList.add('scroll')
+  }
+  
 })
